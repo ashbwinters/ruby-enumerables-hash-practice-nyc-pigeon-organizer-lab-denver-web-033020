4f98c8ba1c530dc31ac9402b3c8692017a9de57e
Ashley Winters <ashbwinters@gmail.com>
2020-03-23 01:39:47 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data_by_pigeon = {}
+  pigeon_names = data[:gender][:male] + data[:gender][:female]
+  pigeon_names.each_with_index{|item|
+    data_by_pigeon[item] = {
+      color: [],
+      gender: [],
+      lives: []
+      }
+    }
+    
+  data.each do |key, value|
+     value.each do |attibutes, pigeon_names|
+       pigeon_names.each do |name|
+         data_by_pigeon[name][key] << attibutes.to_s
+       end
+     end
+   end
+   return data_by_pigeon
 end
