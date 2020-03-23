108acb5275e88a5f41330022cdeb0dc533642758
Ashley Winters <ashbwinters@gmail.com>
2020-03-23 00:38:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data_by_pigeon = {}
+  pigeon_names = data[:gender][:male] + data[:gender][:female]
+  pigeon_names.each_with_index{|item|
+    data_by_pigeon[item] = {
+      color: [],
+      gender: [],
+      lives: []}
+    }
+  data_by_pigeon = data.each_pair do |pair|
+     pair 
+    
+    memo  
+    end
 end
