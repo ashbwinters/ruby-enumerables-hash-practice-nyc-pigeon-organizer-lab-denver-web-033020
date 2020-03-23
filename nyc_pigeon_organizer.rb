9c0e04020c88e5934de1938f91e0fc1ee38a69d0
Ashley Winters <ashbwinters@gmail.com>
2020-03-23 00:54:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,8 @@
 def nyc_pigeon_organizer(data)
   data_by_pigeon = {}
   data_by_pigeon = data.each do |key, value|
-    value.each do |new_value|
-      if 
+    value.each do |new_value, name|
+      if name 
     end
   end
 end
